100d3673cfc87e1ba31592c9a3c6a85ace54dd22
Volodymyr Kovtun <volodymyr_kovtun@epam.com>
2026-02-24 23:09:55 +0200

    complete h/w with stdio

--- agent/app.py
+++ agent/app.py
@@ -6,6 +6,12 @@ from agent.mcp_client import MCPClient
 from agent.dial_client import DialClient
 from agent.models.message import Message, Role
 
+DIAL_ENDPOINT = "https://ai-proxy.lab.epam.com"
+API_KEY = os.getenv('DIAL_API_KEY')
+
+
+SYSTEM_PROMPT = """You are a helpfull assistent that help people using MCP features"""
+
 async def main():
 
     #TODO:
@@ -18,7 +24,43 @@ async def main():
     #       - mcp_client=mcp_client
     # 4. Create list with messages and add there SYSTEM_PROMPT with instructions to LLM
     # 5. Create console chat (infinite loop + ability to exit from chat + preserve message history after the call to dial client)
-    raise NotImplementedError()
+    # raise NotImplementedError()
+
+    async with MCPClient(docker_image="mcp/duckduckgo:latest") as mcp_client:
+        resources = await mcp_client.get_resources()
+        print(f'mcp resources: {resources}\n')
+
+        tools = await mcp_client.get_tools()
+        print(f'mcp tools: {tools}\n')
+
+        dial_client = DialClient(api_key=API_KEY, endpoint=DIAL_ENDPOINT, tools=tools, mcp_client=mcp_client)
+
+        messages = [Message(role=Role.SYSTEM, content=SYSTEM_PROMPT)]
+
+        prompt_results = await mcp_client.get_prompts()
+        if prompt_results:
+            for prompt in prompt_results.prompts:
+                content = await mcp_client.get_prompt(prompt.name)
+                messages.append(
+                    Message(
+                        role=Role.USER,
+                        content=f'## Prompt provided by MCP server: {prompt.description}\n{content}')
+                )
+
+        print('Ask the question')
+        while True:
+            user_question = input('> ').strip()
+
+            if user_question == 'exit':
+                print('Exiting')
+                break
+
+            messages.append(Message(role=Role.USER, content=user_question))
+            ai_response = await dial_client.get_completion(messages)
+
+            print('api response:', ai_response)
+
+            messages.append(ai_response)
 
 
 if __name__ == "__main__":
--- agent/mcp_client.py
+++ agent/mcp_client.py
@@ -29,6 +29,11 @@ class MCPClient:
         # 5. Init `session` with `await self._session_context.__aenter__()`
         # 6. Call `self.session.initialize()`, and print its result (to check capabilities of MCP server later)
         # 7. return self
+        server_params = StdioServerParameters(command='docker', args=["run", "--rm", "-i", self.docker_image])
+        self._stdio_context = stdio_client(server_params)
+        read_stream, write_stream = await self._stdio_context.__aenter__()
+        self._session_context = ClientSession(read_stream, write_stream)
+        self.session = await self._session_context.__aenter__()
 
         print("Initializing MCP session...")
         init_result = await self.session.initialize()
@@ -41,7 +46,12 @@ class MCPClient:
         # This is shutdown method.
         # If session is present and session context is present as well then shutdown the session context (__aexit__ method with params)
         # If stdio context is present then shutdown the stdio context (__aexit__ method with params)
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        if self.session and self._session_context:
+            await self._session_context.__aexit__(exc_type, exc_val, exc_tb)
+
+        if self._stdio_context:
+            await self._stdio_context.__aexit__(exc_type, exc_val, exc_tb)
 
     async def get_tools(self) -> list[dict[str, Any]]:
         """Get available tools from MCP server"""
@@ -62,7 +72,23 @@ class MCPClient:
         #             }
         #             for tool in tools.tools
         #         ]
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        results = []
+
+        tools_list = await self.session.list_tools()
+        for tool in tools_list.tools:
+            results.append(
+                {
+                    'type': 'function',
+                    'function': {
+                        'name': tool.name,
+                        'description': tool.description,
+                        'parameters': tool.inputSchema,
+                    }
+                }
+            )
+
+        return results
 
     async def call_tool(self, tool_name: str, tool_args: dict[str, Any]) -> Any:
         """Call a specific tool on the MCP server"""
@@ -76,7 +102,15 @@ class MCPClient:
         # 3. print(f"    ⚙️: {content}\n")
         # 4. If `isinstance(content, TextContent)` -> return content.text
         #    else -> return content
-        raise NotImplementedError()
+        # raise NotImplementedError()
+
+        tool_result: CallToolResult = await self.session.call_tool(tool_name, tool_args)
+        content = tool_result.content[0]
+
+        if isinstance(content, TextContent):
+            return content.text
+        else:
+            return content
 
     async def get_resources(self) -> list[Resource]:
         """Get available resources from MCP server"""
@@ -85,7 +119,15 @@ class MCPClient:
         #TODO:
         # Wrap into try/except (not all MCP servers have resources), get `list_resources` (it is async) and resources
         # from it. In case of error print error and return an empty array
-        raise NotImplementedError()
+        # raise NotImplementedError()
+
+        try:
+            resources = await self.session.list_resources()
+        except Exception as e:
+            resources = []
+            print(f'Error while list resources: {str(e)}')
+
+        return resources
 
     async def get_prompts(self) -> list[Prompt]:
         """Get available prompts from MCP server"""
@@ -95,4 +137,11 @@ class MCPClient:
         #TODO:
         # Wrap into try/except (not all MCP servers have prompts), get `list_prompts` (it is async) and prompts
         # from it. In case of error print error and return an empty array
-        raise NotImplementedError()
+        # raise NotImplementedError()
+
+        try:
+            prompts = await self.session.list_prompts()
+        except Exception as e:
+            prompts = []
+            print(f'Error getting prompts: {str(e)}')
+        return prompts
